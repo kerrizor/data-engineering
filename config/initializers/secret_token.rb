ae8323a02f73b4d2933d83b1c6e6a63bd382f848
Kerri Miller <kmiller@bluebox.net>
2013-04-13 11:37:54 -0700

    barebones 'rails new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataEngineering::Application.config.secret_token = '4d0f88b53601fdc94e1887b64e5bd65ff4201ec75fcad0b8f687b3d55a8e5f05a40286fdb264ed90b331cda55f6be75a4c741693e775e7c69f37d3d3c5f8a7c6'
